279f0b3eef348f55eec237a1073df8b48c9d9c27
Tom Roberts <t.roberts@kcl.ac.uk>
2018-11-29 12:08:38 +0000

    Remove/rename files
    Removed some WIP files.

---
+++ poly3D_phase_subtract.m
@@ -0,0 +1,39 @@
+function Pcorr = poly3D_phase_subtract(P,MASK)
+
+%% poly3D_phase_subtract   GENERATE BACKGROUND PHASE SUBTRACTED IMAGES
+%
+% INPUT:
+%       - P             Original phase image
+%       - MASK          mask covering static region in phase image
+%
+% OUTPUT:
+%       - Pcorr         Corrected phase image
+%       - P1            Polynomial surface
+%
+%
+
+% Data Admin
+[nRow,nCol,nSl,nFrame] = size( P );
+M = MASK;
+
+% Estimate Constant Background Phase
+P0 = median(P(find(M)));
+
+% Fit Polynomial to Background Phase
+[xdc,ydc,zdc] = meshgrid(1:nCol,1:nRow,1:nSl);
+% [xdc,ydc,zdc] = meshgrid(voxSize(2)*(1:nCol),voxSize(1)*(1:nRow),voxSize(3)*(1:nSl));
+x = repmat(xdc,[1,1,nFrame]);
+y = repmat(ydc,[1,1,nFrame]);
+z = repmat(zdc,[1,1,nFrame]);
+polyOrder = 3;
+% model = polyfitn( [x(find(M)),y(find(M)),z(find(M))], angle(P(find(M))*exp(-(1i*P0))), polyOrder );
+model = polyfitn( [x(find(M)),y(find(M)),z(find(M))], P(find(M)), polyOrder );
+
+P1 = reshape( polyvaln( model, [xdc(:),ydc(:),zdc(:)] ), [nRow,nCol,nSl] );
+
+% Poly-corrected Phase Image
+Pcorr = P-P1;
+
+
+
+end % poly3D_phase_subtract(...)
